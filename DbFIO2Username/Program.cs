aeb8ce8f2719bf482451f7be17ded55a6d8af6ac
megabspam <megabspam@yandex.ru>
2021-04-20 20:26:15 +0700

    release

--- DbFIO2Username/Program.cs
+++ DbFIO2Username/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace DbFIO2Username
     {
         static void Main(string[] args)
         {
+            StreamReader fs1 = new StreamReader(args[0]);
+            StreamWriter result1 = new StreamWriter(args[0] + ".usernames");
+
+            string s = "";
+            var list = new List<string> { };
+            while (s != null)
+            {
+                s = fs1.ReadLine();
+                if (s != null)
+                {
+                    var s1 = s.Split(' ');
+                    string u = "";
+                    foreach (var z in s1)
+                    {
+                        result1.WriteLine(z);
+                        u += z;
+                    };
+                    result1.WriteLine(u);
+                    u = "";
+                    foreach (var z in s1.Reverse())
+                    {
+                        u += z;
+                    };
+                    result1.WriteLine(u);
+                }
+            }
+            result1.Close();
+
         }
     }
 }
