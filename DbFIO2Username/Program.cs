287e53311dc2b39bcafad81df5a34bbd97fd0b2c
megabspam <megabspam@yandex.ru>
2021-04-20 20:17:37 +0700

    Добавьте файлы проекта.

---
+++ DbFIO2Username/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFIO2Username
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
